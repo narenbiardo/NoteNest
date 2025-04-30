d4df6734eeb959d7149820be6275a51d58d6db9d
Naren Biardo <70331736+narenbiardo@users.noreply.github.com>
2025-04-30 18:23:35 -0300

    Added UpdateCategoryDto

---
+++ backend/Models/UpdateCategoryDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EnsolversChallenge.Models
+{
+    public class UpdateCategoryDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required, StringLength(25)]
+        public string Name { get; set; } = default!;
+
+        [StringLength(50)]
+        public string? Description { get; set; }
+    }
+}
