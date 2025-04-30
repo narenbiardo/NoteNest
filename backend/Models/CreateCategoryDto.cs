13ff90e8fc218c2bd7ccf56d462f2cc1da2c7a43
Naren Biardo <70331736+narenbiardo@users.noreply.github.com>
2025-04-30 18:22:56 -0300

    Added CreateCategoryDto

---
+++ backend/Models/CreateCategoryDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EnsolversChallenge.Models
+{
+    public class CreateCategoryDto
+    {
+        [Required, StringLength(25)]
+        public string Name { get; set; } = default!;
+
+        [StringLength(50)]
+        public string? Description { get; set; }
+    }
+}
